878b36b519ef6eed36b617db63d62fa1bc72873a
Stanislav Láznička <stlaz.devel@proton.me>
2023-02-16 11:28:50 +0100

    requestheaders: add a "requestheader-uid-headers" flag and wire it up

--- pkg/kubeapiserver/authenticator/config.go
+++ pkg/kubeapiserver/authenticator/config.go
@@ -111,6 +111,7 @@ func (config Config) New(serverLifecycle context.Context) (authenticator.Request
 			config.RequestHeaderConfig.CAContentProvider.VerifyOptions,
 			config.RequestHeaderConfig.AllowedClientNames,
 			config.RequestHeaderConfig.UsernameHeaders,
+			config.RequestHeaderConfig.UIDHeaders,
 			config.RequestHeaderConfig.GroupHeaders,
 			config.RequestHeaderConfig.ExtraHeaderPrefixes,
 		)
